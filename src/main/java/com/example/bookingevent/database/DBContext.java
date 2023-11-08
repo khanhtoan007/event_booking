3ae80e046afa41d3e702448551bcbabae1a64a69
TonyToan <toantdkde160402@fpt.edu.vn>
2023-11-08 10:46:24 +0700

    deploy db len azure

--- src/main/java/com/example/bookingevent/database/DBContext.java
+++ src/main/java/com/example/bookingevent/database/DBContext.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class DBContext {
      public Connection getConnection()throws Exception {
-        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+         String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName + ";trustServerCertificate=true;";
         if(instance == null || instance.trim().isEmpty())
             url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -14,12 +14,12 @@ public class DBContext {
     }
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "LAPTOP-99CNG1A4";
+    private final String serverName = "minh.database.windows.net";
     private final String dbName = "EventBooking";
     private final String portNumber = "1433";
     private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
-    private final String userID = "sa";
-    private final String password = "sa";
+    private final String userID = "minh";
+    private final String password = "Matkhaulagivaytroi1";
 //    Test Connect DB
     public static void main(String[] args) {
         try {
